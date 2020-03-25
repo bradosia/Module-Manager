6c05b9a63a66746e941a807e59d891daca646bca
bradosia <bradosia@gmail.com>
2020-03-25 12:16:07 -0700

    Update ModuleManager.hpp

--- src/core/ModuleManager.hpp
+++ src/core/ModuleManager.hpp
@@ -1,13 +1,13 @@
 /*
- * @name Plugin Manager
+ * @name Module Manager
  * @author Branden Lee
  * @version 1.00
  * @license GNU LGPL v3
- * @brief Manages shared library plugins
+ * @brief Manages shared library modules
  */
 
-#ifndef BRADOSIA_PLUGIN_MANAGER_H
-#define BRADOSIA_PLUGIN_MANAGER_H
+#ifndef BRADOSIA_MODULE_MANAGER_H
+#define BRADOSIA_MODULE_MANAGER_H
 
 // c++17
 #include <filesystem>
@@ -34,8 +34,8 @@ template <class T> boost::shared_ptr<T> to_boost(const std::shared_ptr<T> &p) {
 
 class InterfaceMethodsBase {
 public:
-  std::string pluginName;
-  InterfaceMethodsBase(std::string s) { pluginName = s; }
+  std::string moduleName;
+  InterfaceMethodsBase(std::string s) { moduleName = s; }
   ~InterfaceMethodsBase() {}
   virtual void addPath(std::filesystem::path p) = 0;
 };
@@ -44,36 +44,36 @@ template <class T> class InterfaceMethods : public InterfaceMethodsBase {
 public:
   InterfaceMethods(std::string s) : InterfaceMethodsBase(s) {}
   ~InterfaceMethods() {}
-  std::vector<boost::shared_ptr<T>> pluginPtrs;
+  std::vector<boost::shared_ptr<T>> modulePtrs;
   void addPath(std::filesystem::path p) {
     boost::filesystem::path lib_path(p.string().c_str());
     std::cout << "PLUGIN: Loading " << p << "\n";
-    boost::shared_ptr<T> plugin;
+    boost::shared_ptr<T> module;
     try {
-      plugin = boost::dll::import<T>(lib_path, pluginName,
+      module = boost::dll::import<T>(lib_path, moduleName,
                                      boost::dll::load_mode::default_mode);
     } catch (...) {
       std::cout << "PLUGIN: Loading FAILED " << p << "\n";
     }
-    if (plugin) {
+    if (module) {
       std::cout << "PLUGIN: Loading SUCCESS " << p << "\n";
-      pluginPtrs.push_back(plugin);
+      modulePtrs.push_back(module);
     }
   }
 };
 
-class PluginManager {
+class ModuleManager {
 private:
   std::unordered_map<std::string, InterfaceMethodsBase *> interfaceMap;
 
 public:
-  template <class T> void addPluginInterface(std::string pluginName) {
-    InterfaceMethods<T> *interface = new InterfaceMethods<T>(pluginName);
+  template <class T> void addModuleInterface(std::string moduleName) {
+    InterfaceMethods<T> *interface = new InterfaceMethods<T>(moduleName);
     InterfaceMethodsBase *interfaceBase = (InterfaceMethodsBase *)interface;
-    interfaceMap.insert({pluginName, interface});
+    interfaceMap.insert({moduleName, interface});
   }
 
-  void loadPlugins(std::string directoryPathStr) {
+  void loadModules(std::string directoryPathStr) {
     for (auto &p :
          std::filesystem::recursive_directory_iterator(directoryPathStr)) {
       std::cout << "PLUGIN: File Found " << p.path() << "\n";
@@ -87,17 +87,17 @@ public:
     }
   }
 
-  template <class T> std::shared_ptr<T> getPlugin(std::string pluginName) {
+  template <class T> std::shared_ptr<T> getModule(std::string moduleName) {
     InterfaceMethods<T> *interface =
-        dynamic_cast<InterfaceMethods<T> *>(interfaceMap.at(pluginName));
-    if (interface->pluginPtrs.empty()) {
+        dynamic_cast<InterfaceMethods<T> *>(interfaceMap.at(moduleName));
+    if (interface->modulePtrs.empty()) {
       return nullptr;
     }
-    return to_std(interface->pluginPtrs.front());
+    return to_std(interface->modulePtrs.front());
   }
 };
 
 } // namespace bradosia
 
 #endif
-// end BRADOSIA_PLUGIN_MANAGER_H
+// end BRADOSIA_MODULE_MANAGER_H
